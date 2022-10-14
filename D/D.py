ee86fa0adef375e8c8c6d42b56451c3fafc7d790
Maxim N <88425424+MxRight@users.noreply.github.com>
2022-10-14 14:33:20 +0300

    Create D.py

---
+++ D/D.py
@@ -0,0 +1,13 @@
+def brackets(cur, open, closed, n):
+    if len(cur) == 2*n:
+        print(cur)
+        return
+    if open < n:
+        brackets(cur + '(', open + 1, closed, n)
+    if closed < open:
+        brackets(cur + ')', open, closed + 1, n)
+
+def gen(n):
+    brackets("", 0,0, n)
+
+gen(int(input()))
